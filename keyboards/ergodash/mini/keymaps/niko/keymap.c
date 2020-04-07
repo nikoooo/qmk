440b31886ff92ebe187253bbab1cfb05e9a76377
Nikola Sevo <nsevonikola@gmail.com>
2020-04-07 13:16:05 +0200

    Swapped pwr butons

--- keyboards/ergodash/mini/keymaps/niko/keymap.c
+++ keyboards/ergodash/mini/keymaps/niko/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_RBRC,                    KC_EQL,     KC_Y,      KC_U,    KC_I,     KC_O,    KC_P,     KC_BSPC, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_NUBS,                    KC_BSLS,    KC_H,      KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_NO,                      KC_NO,      KC_N,      KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT, \
-    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, MO(_LOWER), KC_SPACE, KC_NO,                      KC_NO,     KC_SPACE,  MO(_RAISE),  KC_RALT,  KC_NO,   KC_RCTL,  KC_RCTL    \
+    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE, MO(_LOWER) , KC_NO,                      KC_NO,    MO(_RAISE),KC_SPACE, KC_RALT,  KC_NO,   KC_RCTL,  KC_RCTL    \
   ),
 
   /* Lower
